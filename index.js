f2b1fb1483de7ba98f8710116b950e3f27f70579
alex <shizofranky@gmail.com>
2024-04-30 13:44:27 +0300

    refactor

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const server = http.createServer(app);
 
 const io = new Server(server , {
     cors:{
-        origin:'http://localhost:3000' ,
+        origin:'*' ,
         methods:['GET' , 'POST'] ,
     }
-});
+});             
 
 io.on("connection" , (socket) => {
 
