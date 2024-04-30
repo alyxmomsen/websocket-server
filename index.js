ae895783adc79ca22c773e26ea4f03205d62d6cc
alex <shizofranky@gmail.com>
2024-04-30 13:14:37 +0300

    refactor

---
+++ index.js
@@ -0,0 +1,57 @@
+const dotenv = require('dotenv') ;
+
+const cors = require('cors') ;
+
+const express = require('express') ;
+const app = express();
+
+app.use(cors());
+
+const {Server} = require('socket.io') ;
+
+const http = require('http');
+const server = http.createServer(app);
+
+const io = new Server(server , {
+    cors:{
+        origin:'http://localhost:3000' ,
+        methods:['GET' , 'POST'] ,
+    }
+});
+
+io.on("connection" , (socket) => {
+
+    socket.on('message' , (message) => {
+
+        console.log(message);
+
+        io.emit('message' , {
+            message:'hello from the server' ,
+        });
+
+        // socket.
+
+        // socket.broadcast.emit('message' , {
+        //     message:'hello from the server' ,
+        // });
+
+    });
+
+    console.log('connection from ' , socket.id);
+});
+
+const PORT = process.env.PORT || 3001 ;
+
+server.listen(PORT , () => {
+    console.log('server run on port : ' + PORT );
+});
+
+const P0RT = 3002 ;
+
+app.listen(P0RT , () => {
+    console.log('hello i am App , i am listen the port : ' , P0RT);
+});
+
+app.get('/' , (req , res) => {
+    res.send('<div class="foo-bar">hello</div>');
+}) ;
